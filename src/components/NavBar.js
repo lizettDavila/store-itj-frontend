a4e8b5e55eb81ce7823811b00f2611bfe6e26c9d
Lizett Enriquez <karidavila0911@gmail.com>
2022-05-16 15:33:54 -0500

    Deleting unused vars

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useLocation, Link, NavLink } from 'react-router-dom';
+import { useLocation, NavLink } from 'react-router-dom';
 import "../styles/navbar.css";
 import { FiShoppingCart } from "react-icons/fi";
 
